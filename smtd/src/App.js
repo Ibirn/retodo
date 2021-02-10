f65e18ed862d7fa92c3e560aa9f6326a3ac2998d
Ibirn <kmcricciuti@gmail.com>
2021-02-09 17:11:39 -0800

    Bones of tabbed lists

--- smtd/src/App.js
+++ smtd/src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Search from "./components/Search";
+import Lists from "./components/Lists";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
       <Search />
+      <Lists />
     </div>
   );
 }
--- smtd/src/components/Lists.jsx
+++ smtd/src/components/Lists.jsx
@@ -1,4 +1,35 @@
 import React from "react";
+import Tab from "react-bootstrap/Tab";
+import Nav from "react-bootstrap/Nav";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Search(props) {
-  return ()
+  return (
+    <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+      <Row>
+        <Col sm={3}>
+          <Nav variant="pills" className="flex-column">
+            <Nav.Item>
+              <Nav.Link eventKey="first">Tab 1</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="second">Tab 2</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+        <Col sm={9}>
+          <Tab.Content>
+            <Tab.Pane eventKey="first">
+              <p>A butt</p>
+            </Tab.Pane>
+            <Tab.Pane eventKey="second">
+              <p>TWO butts</p>
+            </Tab.Pane>
+          </Tab.Content>
+        </Col>
+      </Row>
+    </Tab.Container>
+  );
+}
